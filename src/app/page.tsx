447cbdc0360359987df13460ba4c67e7c04286ad
Tri <trimpham98@gmail.com>
2025-03-01 21:32:46 -0800

    Reduce word count requirement to 100

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Header />
       <main className="py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <WritingEditor targetWordCount={500} />
+          <WritingEditor targetWordCount={100} />
           <div className="mt-8">
             <ContributionGraph />
           </div>
